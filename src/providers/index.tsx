49eb1bdcdeddd6eaf3f4746239d4d03ece5008ae
Christian Velez <christian.gerardo_@outlook.es>
2025-03-16 18:28:38 -0600

    adjust theme provider

--- src/providers/index.tsx
+++ src/providers/index.tsx
@@ -1,9 +1,15 @@
+'use client'
 import { Toaster } from '@/components/toaster'
 import { ThemeProvider as NextThemesProvider } from 'next-themes'
 
 export function AppProvider({ children }: { children: React.ReactNode }) {
    return (
-      <NextThemesProvider attribute='class' enableSystem>
+      <NextThemesProvider
+         attribute='class'
+         defaultTheme='system'
+         enableSystem
+         disableTransitionOnChange
+      >
          <Toaster />
 
          {children}
